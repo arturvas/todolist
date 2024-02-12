0c86286142b6c3164cc86f8d71fbe5b48d43276b
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-12 15:30:47 -0400

    feat: add lógica de estado, implementa funcao getTarefasPendentes e add lista de tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const estado = reactive({
+  filtro: "todas",
+  tarefas: [
+    {
+      titulo: "Estudar ES6",
+      finalizada: false,
+    },
+    {
+      titulo: "Estudar SASS",
+      finalizada: false,
+    },
+    {
+      titulo: "Ir para a academia",
+      finalizada: true,
+    },
+  ],
+});
+
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter((tarefa) => !tarefa.finalizada);
+};
+</script>
 
 <template>
   <div class="container">
     <header class="p-5 mb-4 mt-4 bg-light rounded-3">
       <h1>Minhas tarefas</h1>
-      <p>Você possui 7 tarefas pendentes</p>
+      <p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
     </header>
     <form action="">
       <div class="row">
@@ -19,8 +43,11 @@
           <button class="btn btn-primary">Cadastrar</button>
         </div>
         <div class="col-md-2">
-          <select class="form-control">
-            <option value="totas">Todas tarefas</option>
+          <select
+            @change="(evento) => (estado.filtro = evento.target.value)"
+            class="form-control"
+          >
+            <option value="Todas">Todas tarefas</option>
             <option value="Pendentes">Pendentes</option>
             <option value="Finalizadas">Finalizadas</option>
           </select>
@@ -28,12 +55,26 @@
       </div>
     </form>
     <ul class="list-group mt-4">
-      <li class="list-group-item">
-        <input type="checkbox" />
-        <label class="ms-3" for="">Estudar ES6</label>
+      <li class="list-group-item" v-for="tarefa in estado.tarefas">
+        <input
+          :checked="tarefa.finalizada"
+          :id="tarefa.titulo"
+          type="checkbox"
+        />
+        <label
+          :class="{ done: tarefa.finalizada }"
+          class="ms-3"
+          :for="tarefa.titulo"
+        >
+          {{ tarefa.titulo }}
+        </label>
       </li>
     </ul>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
