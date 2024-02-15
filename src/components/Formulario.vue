e59a33c9d144c5928616e78e8a0f058a08979bb2
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-14 23:23:52 -0400

    add componente

---
+++ src/components/Formulario.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps([
+  "cadastraTarefa",
+  "tarefaTemp",
+  "trocarFiltro",
+  "editaTarefaTemp",
+]);
+</script>
+
+<template>
+  <form @submit.prevent="props.cadastraTarefa">
+    <div class="row">
+      <div class="col-md-8 mb-2">
+        <input
+          :value="props.tarefaTemp"
+          @change="props.editaTarefaTemp"
+          required
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui a descrição da tarefa"
+        />
+      </div>
+      <div class="col-md-4 d-flex">
+        <div class="col-md-auto me-3">
+          <button class="btn btn-outline-primary">Add +</button>
+        </div>
+        <div class="col">
+          <select @change="props.trocarFiltro" class="form-select">
+            <option value="Todas">Todas</option>
+            <option value="Pendentes">Pendentes</option>
+            <option value="Finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps(["tarefas"]);
+</script>
+
+<template>
+  <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in props.tarefas">
+      <input
+        @change="(evento) => (tarefa.finalizada = evento.target.checked)"
+        :checked="tarefa.finalizada"
+        :id="tarefa.titulo"
+        type="checkbox"
+      />
+      <label
+        :class="{ done: tarefa.finalizada }"
+        class="ms-3"
+        :for="tarefa.titulo"
+      >
+        {{ tarefa.titulo }}
+      </label>
+    </li>
+  </ul>
+  <div v-else class="alert alert-success mt-4">
+    <strong>Parabéns! Não existem tarefas pendentes.</strong>
+  </div>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
